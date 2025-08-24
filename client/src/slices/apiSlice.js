100e96d3549dae438ea35cd769c304c88ee7469e
ilyesDev31 <ibendjellal8@gmail.com>
2025-08-24 19:02:32 +0100

    third commit

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,6 +1,7 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+const url = import.meta.env.VITE.BASE_URL;
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3000/",
+  baseUrl: url,
   credentials: "include",
 });
 
