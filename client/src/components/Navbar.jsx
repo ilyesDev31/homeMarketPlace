e95d722297cf69addc59f0b88e04214029e4bd50
ilyesDev31 <ibendjellal8@gmail.com>
2025-08-24 20:25:28 +0100

    seventh commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { NavLink, Link } from "react-router-dom";
-import { ReactComponent as OfferIcon } from "../assets/svg/LocalOfferIcon.svg";
+import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg";
 import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg";
 import { ReactComponent as PersonIcon } from "../assets/svg/PersonOutlineIcon.svg";
 const Navbar = () => {
